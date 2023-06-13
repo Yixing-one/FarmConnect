9355e59087f759f1ef7322868315a1407d6d2803
Mahdi Rahman <35085896+mahdirahman1@users.noreply.github.com>
2023-06-13 01:19:19 -0400

    add icon for side drawer

--- app/src/main/java/com/example/farmconnect/MainActivity.kt
+++ app/src/main/java/com/example/farmconnect/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.farmconnect.ui.AppBar
 import com.example.farmconnect.ui.theme.FarmConnectTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,25 +23,37 @@ class MainActivity : ComponentActivity() {
             FarmConnectTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                   App("Android")
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun App(name: String, modifier: Modifier = Modifier) {
+    Scaffold( topBar = { AppBar() }) {
+     paddingValues ->
+
+        Surface(modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues)){
+            Text(
+                text = "Hello $name!",
+                modifier = modifier
+            )
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun AppPreview() {
     FarmConnectTheme {
-        Greeting("Android")
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+            App("Android")
+        }
     }
 }
