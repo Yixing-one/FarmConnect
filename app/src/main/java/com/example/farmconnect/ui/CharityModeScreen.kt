a34053c107e1124f76d2dbb6759e9e3106064a3a
Yixing-one <y3536zha@uwaterloo.ca>
2023-06-25 16:21:04 -0400

    Add the ui for charity donation page

--- app/src/main/java/com/example/farmconnect/ui/CharityModeScreen.kt
+++ app/src/main/java/com/example/farmconnect/ui/CharityModeScreen.kt
@@ -1,9 +1,165 @@
 package com.example.farmconnect.ui
 
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.farmconnect.R
+import com.example.farmconnect.ui.theme.FarmConnectTheme
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 
+
+data class Post(
+    val charity_name: String,
+    val charity_location: String,
+    val charity_distance: Double,
+    val item_name: String,
+    val item_amount: Double,
+    @DrawableRes
+    val imageId: Int
+){
+    fun doesMatchSearchQuery(query: String): Boolean {
+        val matchingCombinations = listOf(
+            "$charity_name", "$charity_location", "$item_name",
+        )
+        return matchingCombinations.any {
+            it.contains(query, ignoreCase = true)
+        }
+    }
+}
+
+private val allPosts = listOf(
+    Post(
+        charity_name = "Charity_ONE",
+        charity_location = "University Ave #1, Waterloo, Canada",
+        charity_distance = 5.0,
+        item_name = "Carrots",
+        item_amount = 3.0,
+        imageId = R.drawable.carrot
+    ),
+)
+
+class FarmViewModel: ViewModel() {
+    private val _searchText = MutableStateFlow("")
+    val searchText = _searchText.asStateFlow()
+
+    private val _isSearching = MutableStateFlow(false)
+    val isSearching = _isSearching.asStateFlow()
+
+    private val _posts = MutableStateFlow(allPosts)
+    val posts = searchText
+        .combine(_posts){ text, posts ->
+            if(text.isBlank()){
+                posts
+            }
+            else{
+                posts.filter{
+                    it.doesMatchSearchQuery(text)
+                }
+            }
+        }
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5000),
+            _posts.value
+        )
+
+    fun onSearchTextChange(text: String){
+        _searchText.value = text
+    }
+}
 @Composable
-fun CharityModeScreen(){
-    Text(text = "Charity Mode screen")
+fun PostCard(post:Post, modifier: Modifier = Modifier){
+    Card(
+        modifier = modifier,
+    ) {
+        Column {
+            Image(
+                painter = painterResource(id = post.imageId),
+                contentDescription = "image",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(120.dp),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = "${post.charity_name}",
+                modifier = Modifier.padding(start = 13.dp, end = 10.dp, top = 10.dp, bottom = 7.dp),
+                style = MaterialTheme.typography.titleMedium,
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CharityScreen(){
+    val viewModel = viewModel<FarmViewModel>()
+    val CharityPosts by viewModel.posts.collectAsState()
+    val searchText by viewModel.searchText.collectAsState()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Row{
+            TextField(
+                value = searchText,
+                onValueChange = viewModel::onSearchTextChange,
+                modifier = Modifier.fillMaxWidth(),
+                placeholder = {Text(text = "Search")},
+            )
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        LazyVerticalGrid(columns = GridCells.Adaptive(minSize = 128.dp)){
+            items(CharityPosts.size){item ->
+                PostCard(
+                    post = CharityPosts.get(item),
+                    modifier = Modifier.padding(8.dp)
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CharityModeScreenPreview() {
+    FarmConnectTheme {
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+            CharityScreen()
+        }
+    }
 }
--- app/src/main/java/com/example/farmconnect/ui/FarmModeScreen.kt
+++ app/src/main/java/com/example/farmconnect/ui/FarmModeScreen.kt
@@ -1,9 +1,181 @@
 package com.example.farmconnect.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Card
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.farmconnect.R
+import com.example.farmconnect.Screens
+import com.example.farmconnect.ui.theme.FarmConnectTheme
+import com.example.farmconnect.ui.theme.darkGreen
 
 @Composable
-fun FarmModeScreen(){
-    Text(text = "Farm Mode screen")
+fun farmModeHeader() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(darkGreen)
+            .height(100.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Farm Mode",
+            color = Color.White,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+        )
+    }
+}
+
+data class PanelItem(
+    val title: String,
+    val description: String,
+    val icon: Painter
+)
+
+@Composable
+fun PanelGrid(
+    panelItems: List<PanelItem>,
+    onItemClick: (PanelItem) -> Unit
+) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        itemsIndexed(panelItems) { _, item ->
+            PanelItemCard(panelItem = item, onItemClick = onItemClick)
+        }
+    }
+}
+
+@Composable
+fun PanelItemCard(
+    panelItem: PanelItem,
+    onItemClick: (PanelItem) -> Unit
+) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .clickable { onItemClick(panelItem) }
+            .fillMaxWidth(),
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxSize()
+        ) {
+            Image(
+                painter = panelItem.icon, // Use the icon property as the painter
+                contentDescription = "Photo",
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(bottom = 8.dp)
+            )
+
+            Divider(color = Color.Black, thickness = 1.dp, modifier = Modifier.padding(bottom = 8.dp))
+
+            Text(
+                text = panelItem.title,
+                fontWeight = FontWeight.Bold
+            )
+            Text(text = panelItem.description)
+        }
+    }
+}
+
+@Composable
+fun getFarmerModeIcons(title: String): Painter {
+    return when (title) {
+        "Finance" -> painterResource(id = R.drawable.baseline_agriculture_24)
+        "Marketplace" -> painterResource(id = R.drawable.baseline_back_hand_24)
+        else -> painterResource(id = R.drawable.baseline_back_hand_24) // Replace with a default icon if needed
+    }
+}
+
+@Composable
+fun getFarmerModeDescription(title: String): String {
+    return when (title) {
+        "Finance" -> "Check out your finances"
+        "Marketplace" -> "Buy or sell items"
+        else -> "Check your inventory"
+    }
+}
+
+@Composable
+fun farmModeScreenNav(navController: NavController) {
+    val currentRoute = navController.currentBackStackEntry?.destination?.route
+
+    val screenMap: HashMap<String, List<String>> = hashMapOf(
+        Screens.Farm.name to listOf(Screens.Finance.name, Screens.Marketplace.name, Screens.Inventory.name),
+        Screens.Charity.name to listOf(Screens.Charity.name),
+        Screens.Settings.name to listOf(Screens.Settings.name)
+    )
+
+    val farmChildScreens = screenMap[Screens.Farm.name] ?: emptyList()
+
+    PanelGrid(
+        panelItems = farmChildScreens.map { screenName ->
+            val icon = getFarmerModeIcons(screenName)
+            val description = getFarmerModeDescription(screenName)
+            PanelItem(screenName, description, icon)
+        },
+        onItemClick = { panelItem ->
+            navController.navigate(panelItem.title)
+        }
+    )
+}
+
+@Composable
+fun FarmModeScreen(navController: NavController) {
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        farmModeHeader()
+        Spacer(modifier = Modifier.height(16.dp))
+        farmModeScreenNav(navController)
+    }
+}
+
+@Preview
+@Composable
+fun FarmModeScreenPreview() {
+    FarmConnectTheme {
+        val navController = rememberNavController()
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+            FarmModeScreen(navController)
+        }
+    }
 }
