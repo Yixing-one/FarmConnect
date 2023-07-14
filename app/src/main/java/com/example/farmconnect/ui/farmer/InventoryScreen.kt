4d7103a49b9f6b31310ef304f5d621e954280274
muhammadabdullah <muhammadabdullahlm10hd@gmail.com>
2023-07-13 21:52:16 -0400

    inventory screen now pulls data from firestore db

--- app/src/main/java/com/example/farmconnect/ui/farmer/InventoryScreen.kt
+++ app/src/main/java/com/example/farmconnect/ui/farmer/InventoryScreen.kt
@@ -1,6 +1,12 @@
 package com.example.farmconnect.ui.farmer
 
 import android.Manifest
+import android.app.Application
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.annotation.DrawableRes
@@ -31,9 +37,13 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
 import com.example.farmconnect.R
 import com.example.farmconnect.SpeechRecognizerContract
 import com.example.farmconnect.ui.theme.FarmConnectTheme
@@ -42,19 +52,80 @@ import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
-import com.example.farmconnect.data.allItems
+//import com.example.farmconnect.data.allItems
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.Filter
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import java.util.Objects
 
-class MainViewModel: ViewModel() {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    val db = Firebase.firestore
+    val storage = Firebase.storage
+    val currentUserId = FirebaseAuth.getInstance().currentUser?.uid.toString();
+    val invItems = ArrayList<Item>()
+
+    val inventoryRef = db.collection("inventory");
+    val inventoryItems = inventoryRef.where(Filter.equalTo("userId", currentUserId))
+        .get().addOnSuccessListener { documents ->
+            for (document in documents) {
+                val docData = document.data;
+                // Create a storage reference from our app
+                val storageRef = storage.reference
+                Log.d(TAG, docData.getValue("imageUrl").toString())
+                // Create a reference with an initial file path and name
+                val imageRef = storageRef.child(docData.getValue("imageUrl").toString())
+
+                val TEN_MEGABYTE:Long = 10000000;
+
+                imageRef.getBytes(TEN_MEGABYTE).addOnSuccessListener { bytes ->
+                    // Data for "images/island.jpg" is returns, use this as needed
+                    val imageBitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+
+                    Log.d(TAG, imageBitmap.width.toString())
+                    Log.d(TAG, imageBitmap.height.toString())
+
+                    Log.d(TAG, docData.keys.toString());
+
+                    Log.d(TAG, docData.getValue("name").toString());
+                    Log.d(TAG, (docData.getValue("price").toString().toDouble()).toString());
+                    Log.d(TAG, (docData.getValue("quantity").toString().toInt()).toString());
+
+
+                    invItems.add(
+                        Item(
+                            name = docData.getValue("name").toString(),
+                            price = docData.getValue("price").toString().toDouble(),
+                            quantity = docData.getValue("quantity").toString().toInt(),
+                            imageBitmap = imageBitmap
+                        )
+                    )
+
+
+                    _items.value = invItems.toList()
+                }
+
+            }
+
+
+    };
+
+
+
     private val _searchText = MutableStateFlow("")
     val searchText = _searchText.asStateFlow()
 
     private val _isSearching = MutableStateFlow(false)
     val isSearching = _isSearching.asStateFlow()
 
-    private val _items = MutableStateFlow(allItems)
+//    private val _items = MutableStateFlow(invItems)
+    private val _items = MutableStateFlow<List<Item>>(listOf())
+
     val items = searchText
         .combine(_items){ text, items ->
             if(text.isBlank()){
@@ -81,7 +152,7 @@ data class Item(
     val name: String,
     val price: Double,
     val quantity: Int,
-    @DrawableRes val imageId: Int
+    val imageBitmap: Bitmap
 ) {
     fun doesMatchSearchQuery(query: String): Boolean {
         val matchingCombinations = listOf(
@@ -98,12 +169,10 @@ data class Item(
 @Composable
 fun ItemCard(item: Item, modifier: Modifier = Modifier){
 
-    val permissionState = rememberPermissionState(
-        permission = Manifest.permission.RECORD_AUDIO
-        )
-        SideEffect {
-            permissionState.launchPermissionRequest()
-        }
+    val permissionState = rememberPermissionState(permission = Manifest.permission.RECORD_AUDIO)
+    SideEffect {
+        permissionState.launchPermissionRequest()
+    }
 
     val speechRecognizerLauncher = rememberLauncherForActivityResult(
         contract = SpeechRecognizerContract(),
@@ -129,7 +198,7 @@ fun ItemCard(item: Item, modifier: Modifier = Modifier){
     ) {
         Column{
            Image(
-                painter = painterResource(id = item.imageId),
+                painter = rememberAsyncImagePainter(model = item.imageBitmap),
                 contentDescription = "image",
                 modifier = Modifier
                     .fillMaxWidth()
@@ -160,6 +229,8 @@ fun InventoryScreen(){
     val viewModel = viewModel<MainViewModel>()
     val searchText by viewModel.searchText.collectAsState()
     val theFoodItems by viewModel.items.collectAsState()
+    Log.d(TAG, "Inside compose")
+    Log.d(TAG, theFoodItems.toString())
 
     Column(
         modifier = Modifier
