349f39eca6582a0f3470407f84df390ed6a4768d
muhammadabdullah <muhammadabdullahlm10hd@gmail.com>
2023-07-18 13:59:24 -0400

    farmer now able to add new item to inventory

--- app/src/main/java/com/example/farmconnect/view/HomePage.kt
+++ app/src/main/java/com/example/farmconnect/view/HomePage.kt
@@ -1,22 +1,56 @@
 package com.example.farmconnect.view
 
+import android.app.Activity.RESULT_OK
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.CheckboxDefaults.colors
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -38,7 +72,13 @@ import com.example.farmconnect.ui.farmer.MainViewModel
 import com.example.farmconnect.ui.shopping.CartScreen
 import com.example.farmconnect.ui.shopping.CartViewModel
 import com.example.farmconnect.ui.shopping.ShoppingCenterScreen
+import com.example.farmconnect.ui.theme.lightGreen
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import kotlinx.coroutines.launch
+import java.io.ByteArrayOutputStream
 
 class HomePage : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -72,6 +112,154 @@ enum class Screens(@StringRes val title: Int) {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
+private fun ExtendedFABComponent() {
+    val showDialog = remember { mutableStateOf(false) }
+    val capturedImage = remember { mutableStateOf<Bitmap?>(null) }
+    val itemName = remember { mutableStateOf("") }
+    val itemQuantity = remember { mutableStateOf("") }
+    val itemPrice = remember { mutableStateOf("") }
+
+
+    val cameraLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val imageBitmap = result.data?.extras?.get("data") as? Bitmap
+            showDialog.value = true
+            capturedImage.value = imageBitmap
+        }
+    }
+
+    fun addItemToFirestore(imageBitmap: Bitmap) {
+        // add captured image to Firestore Storage
+        val storage = Firebase.storage
+        val storageRef = storage.reference
+        val imagesRef = storageRef.child("images/crops")
+        val fileName = "image_${System.currentTimeMillis()}.jpg"
+        val imageRef = imagesRef.child(fileName)
+
+        val baos = ByteArrayOutputStream()
+        imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val imageData = baos.toByteArray()
+
+        val uploadTask = imageRef.putBytes(imageData)
+        uploadTask.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                // Image uploaded successfully
+                // Create a new doc in the inventory collection
+                val db = Firebase.firestore
+                val inventoryColRef = db.collection("inventory");
+                val data = hashMapOf(
+                    "name" to itemName.value,
+                    "quantity" to itemQuantity.value,
+                    "price" to itemPrice.value,
+                    "userId" to FirebaseAuth.getInstance().currentUser?.uid.toString(),
+                    "imageUrl" to imageRef.path.toString()
+                )
+                inventoryColRef.add(data);
+            }
+
+            // clear input values
+            itemName.value = ""
+            itemQuantity.value = ""
+            itemPrice.value = ""
+        }
+    }
+
+    if (showDialog.value) {
+        AlertDialog(
+            onDismissRequest = { showDialog.value = false },
+            title = { Text("Add Item to inventory") },
+            text = {
+                Column {
+                    capturedImage.value?.let {
+                        Image(
+                            bitmap = it.asImageBitmap(),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(200.dp)
+                                .align(Alignment.CenterHorizontally)
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    TextField(
+                        value = itemName.value,
+                        onValueChange = { itemName.value = it },
+                        label = { Text("Enter Name") },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    TextField(
+                        value = itemQuantity.value,  // This should be a mutable state of String
+                        onValueChange = {
+                            if (it.toFloatOrNull() != null || it.isEmpty()) {
+                                itemQuantity.value = it
+                            }
+                        },
+                        label = { Text("Enter quantity") },
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    TextField(
+                        value = itemPrice.value,  // This should be a mutable state of String
+                        onValueChange = {
+                            if (it.toFloatOrNull() != null || it.isEmpty()) {
+                                itemPrice.value = it
+                            }
+                        },
+                        label = { Text("Enter price") },
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                        modifier = Modifier.fillMaxWidth()
+                    )
+
+                }
+            },
+            confirmButton = {
+                Button(onClick = {
+                    // Upload the captured image to Firestore Storage
+                    capturedImage.value?.let { imageBitmap ->
+                        addItemToFirestore(imageBitmap)
+                    }
+
+                    // Close the dialog
+                    showDialog.value = false
+                }) {
+                    Text("OK")
+                }
+            }
+        )
+    }
+
+    val context = LocalContext.current
+    FloatingActionButton(
+        onClick = { takePicture(context, cameraLauncher) },
+        content = {
+            Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier
+                .padding(horizontal = 10.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add Item",
+                    modifier = Modifier.padding(end = 4.dp)
+                )
+                Text("Add Item")
+            }
+        }
+
+    )
+}
+
+private fun takePicture(context: Context, cameraLauncher: ActivityResultLauncher<Intent>) {
+    val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+    if (cameraIntent.resolveActivity(context.packageManager) != null) {
+        cameraLauncher.launch(cameraIntent)
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
 fun App() {
     val navController = rememberNavController()
     val backStackEntry by navController.currentBackStackEntryAsState()
@@ -94,7 +282,9 @@ fun App() {
                 val showShoppingCart = currentScreen == Screens.Shopping
                 val showCloseIcon = currentScreen == Screens.Cart
                 AppBar(onMenuClick = { scope.launch { drawerState.open() } }, showShoppingCart, showCloseIcon, navController)
-            }
+            },
+            floatingActionButton = { ExtendedFABComponent()},
+            floatingActionButtonPosition = FabPosition.End
         ) {
                 paddingValues ->
 
@@ -103,43 +293,43 @@ fun App() {
                 .padding(paddingValues)
                 .padding(horizontal = 5.dp, vertical = 5.dp)
             ){
+                Column() {
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screens.Farm.name
 
-                NavHost(
-                    navController = navController,
-                    startDestination = Screens.Farm.name
-
-                ){
+                    ){
 //                  Farm mode:
-                    composable(route = Screens.Farm.name){
-                        FarmModeScreen(navController)
-                    }
-                    composable(route = Screens.Inventory.name){
-                        InventoryScreen()
-                    }
-                    composable(route = Screens.Finance.name){
-                        FinanceStatsScreen()
-                    }
-                    composable(route = Screens.Marketplace.name){
-                        MarketplaceScreen()
-                    }
+                        composable(route = Screens.Farm.name){
+                            FarmModeScreen(navController)
+                        }
+                        composable(route = Screens.Inventory.name){
+                            InventoryScreen()
+                        }
+                        composable(route = Screens.Finance.name){
+                            FinanceStatsScreen()
+                        }
+                        composable(route = Screens.Marketplace.name){
+                            MarketplaceScreen()
+                        }
 //                  Charity mode:
-                    composable(route = Screens.Charity.name){
-                        CharityModeScreen()
-                    }
+                        composable(route = Screens.Charity.name){
+                            CharityModeScreen()
+                        }
 //                  E-commerce center:
-                    composable(route = Screens.Shopping.name){
-                        ShoppingCenterScreen(cartViewModel)
-                    }
-                    composable(route = Screens.Cart.name){
-                        CartScreen(cartViewModel)
-                    }
+                        composable(route = Screens.Shopping.name){
+                            ShoppingCenterScreen(cartViewModel)
+                        }
+                        composable(route = Screens.Cart.name){
+                            CartScreen(cartViewModel)
+                        }
 //                    Settings
-                    composable(route = Screens.Settings.name){
-                        SettingsScreen()
+                        composable(route = Screens.Settings.name){
+                            SettingsScreen()
+                        }
                     }
-
-
                 }
+
             }
         }
     }
